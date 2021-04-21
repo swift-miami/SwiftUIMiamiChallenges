23fe1a238ae69cc5af3bc64486eedcd5758aac44
Dylan McCarthy <mccarthy.dylan@gmail.com>
2021-04-21 10:23:33 -0700

    Building Progress Circle

---
+++ Miami SwiftUI Challenges/2021/2021-05/04-21-2021.swift
@@ -0,0 +1,20 @@
+//
+//  04-21-2021.swift
+//  SwiftUI Miami Challenges
+//
+//  Created by Dylan  on 4/21/21.
+//
+
+import SwiftUI
+
+struct _4_21_2021: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _4_21_2021_Previews: PreviewProvider {
+    static var previews: some View {
+        _4_21_2021()
+    }
+}
